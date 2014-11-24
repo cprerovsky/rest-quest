0c71c1e6870e80f9600650f5beb0e87527c44590
Clemens Prerovsky <c.prerovsky@gentics.com>
2014-11-24 17:42:28 +0100

    refactoring

--- server.js
+++ server.js
@@ -1,64 +1,20 @@
 /*global require,console*/
 'use strict';
 
-var Express = require('express.io');
+var Express    = require('express.io');
 var BodyParser = require('body-parser');
-var Assert = require('./lib/assert');
-var Maps = require('./lib/maps');
-var Player = require('./lib/player');
-var both = require('./lib/both');
+var Maps       = require('./lib/maps');
+var Game       = require('./lib/game');
 
+var State = Game.state();
 var App = new Express();
-var State = initState();
 
 App.use(BodyParser.urlencoded({ extended: true }));
 
-
-// ----- funcs -----
-
-function initState () {
-	return {
-		players : {},
-		map     : Maps.create()
-	};
-}
-
 // ----- routes -----
 
-App.post('/register/', function (req, res) {
-	Assert.registryOpen(State.players);
-
-	var player = Player.create(req.body.name, State.players);
-	State.players[player.name] = player;
-	
-	var castlePlace = Maps.placementTile(State.map);
-	castlePlace.tile.castle = player.name;
-	player.pos = {
-		x: castlePlace.x,
-		y: castlePlace.y
-	};
-
-	console.log('registered player {' + player.name + '} at {' + player.pos.x + ',' + player.pos.y + '}');
-	
-	both('register', function () {
-		
-		res.send('OK');
-	}).once(function () { console.log('once!'); });
-});
-
-App.post('/move/', function (req, res) {
-	Assert.registryClosed(State.players);
-	var player = Player.get(req.body.player);
-	var newPos = Maps.move(player.pos, req.body.direction, State.map.size);
-	player.pos = newPos;
-	player.turn++;
-
-	console.log('moved player {' + player.name + '} to {' + player.pos.x + ',' + player.pos.y + '} in turn {' + player.turn + '}');
-
-	setTimeout(function () {
-		res.send(JSON.stringify(Maps.view(player.pos, State.map)));
-	}, 500);
-});
+App.post('/register/', function (req, res) { Game.register(State, req, res); });
+App.post('/move/',     function (req, res) { Game.move(State, req, res); });
 
 App.listen(3000);
 console.log('Server started on localhost:3000');
