a3c2c2c53084281208c15fb54e0a1129d4793a1b
Clemens Prerovsky <c.prerovsky@gentics.com>
2014-12-01 17:55:03 +0100

    improved client

--- client.js
+++ client.js
@@ -1,58 +1,79 @@
 /*global require,console*/
 'use strict';
 
-var Request = require('request');
-
-var Player = { name : 'clembot-' + parseInt(Math.random() * 1000, 10) };
+var request = require('request');
+var Player = { name : 'clembot-' + parseInt(Math.random() * 100, 10) };
 var BASE_URL = 'http://localhost:3000';
-var DIRECTIONS = ['up', 'right', 'down', 'left'];
 
-var i=0;
-function move() {
-	i++;
-	Request({
+function takeTurn (pos, search) {
+	var oldPos = pos;
+	var dir = search ? nextSearchStep() : nextHomeStep(pos);
+	pos = updatePos(pos, dir);
+	move(dir, function (data) {
+		takeTurn(pos, !data.treasure);
+	});
+}
+
+function nextHomeStep(pos) {
+	if (pos.x > 0) {
+		return 'left';
+	}
+	return 'down';
+}
+
+function updatePos (pos, dir) {
+	var x = pos.x;
+	var y = pos.y;
+
+	if (dir === 'up') {
+		y++;
+	} else if (dir === 'right') {
+		x++;
+	} else if (dir === 'down') {
+		y--;
+	} else if (dir === 'left') {
+		x--;
+	}
+
+	return { x: x, y: y};
+}
+
+function move (dir, cb) {
+	request({
 		uri    : BASE_URL + '/move/',
 		method : 'POST',
 		form   : {
 			player    : Player.name,
-			direction : 'right'
+			direction : dir
 		}		
 	}, function (error, res, body) {
-		console.log(body);
-		if (i<10) {
-				move();
+		var data = JSON.parse(body);
+		if (data.game) {
+			console.log(Player.name, data);
+			reset();
 		} else {
-			Request({ uri: BASE_URL + '/reset/', method : 'GET' });
+			cb(data);
 		}
 	});
 }
 
-function print (str) {
-	var data = JSON.parse(str);
-	var logstr;
-
-	if (!data.view) {
-		console.log(data);
-		return;
-	}
-
-	for (var y=0; y<data.length; y++) {
-		logstr = '';
-		for (var x=0; x<data.length; x++) {
-			logstr += data[y][x].type[0] + ' ';
-		}
-		console.log(logstr);
-	}
-	console.log('\n');
+function nextSearchStep () {
+	return Math.random() >= 0.5 ? 'right' : 'up';
 }
 
-Request({
+function reset () {
+	request({
+		uri    : BASE_URL + '/reset/',
+		method : 'GET',
+	}, function () {});
+}
+
+request({
 	uri    : BASE_URL + '/register/',
 	method : 'POST',
 	form   : {
 		name : Player.name
 	}
-}, function (error, res, body) {
-	console.log(body);
-	move();
+}, function () {
+	takeTurn({ x:0, y:0 }, true);
 });
